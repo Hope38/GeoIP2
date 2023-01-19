011cdddcb481c7239ca2de8b1b533d990d586b23
Hope38 <122560567+Hope38@users.noreply.github.com>
2023-01-18 22:43:01 -0500

    Update GeoIP_Script.sh

--- GeoIP_Script.sh
+++ GeoIP_Script.sh
@@ -1,33 +1,53 @@
-#A script which adds an external APT repository or removes an already existing repository
+#https://medium.com/@maxime.durand.54/add-the-geoip2-module-to-nginx-f0b56e015763
+
+#"sudo" enables users to run programs with the security privileges of another user
+#"apt" is a high-level computer programming language that is used to generate instructions.
+
+#A script which adds an external APT repository or removes an already existing repository - "add-apt-repository [OPTIONS] REPOSITORY"
 #Ubuntu
 #ppa:maxmind/ppa is the repository
 $ sudo add-apt-repository ppa:maxmind/ppa
 
+
 #After adding the repository, update the package cache with packages from this repository
 $ sudo apt update
+
+
+#"libmaxminddb" library provides a C library for reading MaxMind DB files, including the GeoIP2 databases from MaxMind.
+#"mmdb-bin" This package contains the command line utilities to resolve IPs using the libmaxminddb library.
+#"libmaxminddb-dev" This package contains the development files for the libmaxminddb library.
+
 
 #Install the geoipupdate packages from your package manager
 sudo apt install geoipupdate libmaxminddb0 libmaxminddb-dev mmdb-bin
 
-#Create a MaxMind Account and generate a new license key
+#Create a MaxMind Account
 #https://www.maxmind.com/en/accounts/current/license-key
-#In the /etc/GeoIP.conf file, replace "YOUR_ACCOUNT_ID_HERE" & "YOUR_LICENSE_KEY_HERE" with your accound id number and license key
+#Then go to manage licenses key and click on generate license key
+#In the /etc/GeoIP.conf file, replace "YOUR_ACCOUNT_ID_HERE" & "YOUR_LICENSE_KEY_HERE" with your accound id number and license key from your Maxmind account
 #Depending on the license you might need to change the EditionIDs
 
 #update the geoip database
 $ sudo geoipupdate
 
+#"cron" submits, edits, lists, or removes cron jobs. A cron job is a command run by the cron daemon at regularly scheduled intervals. 
+#To submit a cron job, specify the crontab command with the -e flag. 
+
 #add a new cron rulle to enable a daily update
 $ sudo crontab -e
 
 # Run GeoIP database update every thursday at 02:00
-0 2 * * 2 /usr/bin/geoipupdate
+$ 0 2 * * 2 /usr/bin/geoipupdate
 
 #Add the GeoIP2 module to Nginx. This clones the github repository of the module
 $ git clone https://github.com/leev/ngx_http_geoip2_module.git
 
-#Check your Nginx version before downloading 
+#Check your Nginx version before downloading with this command below
 # $ nginx-v
+
+# "tar" means an archiving utility, "z" means unzip, "x" means extract files from archive, "v" means print the filenames verbosely, "f" means the following argument is a filename
+# "wget" means the non-interative network downloader
+# "cd" allows you to change directories
 
 #Download the Nginx corresponding version
 $ wget http://nginx.org/download/nginx-VERSION.tar.gz
@@ -36,8 +56,10 @@ $ cd nginx-VERSION
 
 #Make the module
 $ ./configure --with-compat --add-dynamic-module=../ngx_http_geoip2_module
-
 $ make modules
+
+#"mkdir" makes a new directory
+#"-p" means parents, so when you put it with mkdir it makes a parent directory
 
 #Copy the GeoIP2 module in the Nginx directory
 $ mkdir -p /etc/nginx/modules
